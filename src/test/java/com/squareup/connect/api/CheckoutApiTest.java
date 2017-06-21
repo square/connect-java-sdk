8e71f498fe38d292c1a35a7dde5d00542a0dd105
Francisco Rojas <frojas@squareup.com>
2017-06-21 11:18:29 -0700

    Add country to shipping address in checkout test

--- src/test/java/com/squareup/connect/api/CheckoutApiTest.java
+++ src/test/java/com/squareup/connect/api/CheckoutApiTest.java
@@ -132,7 +132,7 @@ public class CheckoutApiTest extends APITest {
                 .locality("San Francisco")
                 .administrativeDistrictLevel1("CA")
                 .postalCode("94103")
-                .country("US")
+                .country(Address.CountryEnum.US)
                 .firstName("Jane")
                 .lastName("Doe")
             )
