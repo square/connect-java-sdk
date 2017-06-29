ef20c01216b237936a81b4246ea1361621a38f57
Travis CI <no-reply@squareup.com>
2017-06-29 23:18:12 +0000

    From connect-api-specification: Support V1 pagination in Java SDK

--- src/main/java/com/squareup/connect/api/CheckoutApi.java
+++ src/main/java/com/squareup/connect/api/CheckoutApi.java
@@ -81,6 +81,56 @@ public class CheckoutApi {
     String[] localVarAuthNames = new String[] { "oauth2" };
 
     GenericType<CreateCheckoutResponse> localVarReturnType = new GenericType<CreateCheckoutResponse>() {};
-    return apiClient.invokeAPI(localVarPath, "POST", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+    (CompleteResponse<CreateCheckoutResponse>) completeResponse = (CompleteResponse<CreateCheckoutResponse>)apiClient.invokeAPI(localVarPath, "POST", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+    return completeResponse.getData();
       }
+
+  /**
+   * CreateCheckout
+   * Creates a [Checkout](#type-checkout) response that links a &#x60;checkoutId&#x60; and &#x60;checkout_page_url&#x60; that customers can be directed to in order to provide their payment information using a payment processing workflow hosted on connect.squareup.com.
+   * @param locationId The ID of the business location to associate the checkout with. (required)
+   * @param body An object containing the fields to POST for the request.  See the corresponding object definition for field details. (required)
+   * @return CompleteResponse<CreateCheckoutResponse>
+   * @throws ApiException if fails to make API call
+   */
+  public CompleteResponse<CreateCheckoutResponse>createCheckoutWithHttpInfo(String locationId, CreateCheckoutRequest body) throws ApiException {
+    Object localVarPostBody = body;
+    
+    // verify the required parameter 'locationId' is set
+    if (locationId == null) {
+      throw new ApiException(400, "Missing the required parameter 'locationId' when calling createCheckout");
+    }
+    
+    // verify the required parameter 'body' is set
+    if (body == null) {
+      throw new ApiException(400, "Missing the required parameter 'body' when calling createCheckout");
+    }
+    
+    // create path and map variables
+    String localVarPath = "/v2/locations/{location_id}/checkouts"
+      .replaceAll("\\{" + "location_id" + "\\}", apiClient.escapeString(locationId.toString()));
+
+    // query params
+    List<Pair> localVarQueryParams = new ArrayList<Pair>();
+    Map<String, String> localVarHeaderParams = new HashMap<String, String>();
+    Map<String, Object> localVarFormParams = new HashMap<String, Object>();
+
+
+    
+    
+    final String[] localVarAccepts = {
+      "application/json"
+    };
+    final String localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+
+    final String[] localVarContentTypes = {
+      "application/json"
+    };
+    final String localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+    String[] localVarAuthNames = new String[] { "oauth2" };
+
+    GenericType<CreateCheckoutResponse> localVarReturnType = new GenericType<CreateCheckoutResponse>() {};
+    return (CompleteResponse<CreateCheckoutResponse>)apiClient.invokeAPI(localVarPath, "POST", localVarQueryParams, localVarPostBody, localVarHeaderParams, localVarFormParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+  }
 }
