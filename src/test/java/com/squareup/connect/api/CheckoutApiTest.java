ef07845cee621305842957d220684e762c751a0c
Connect API Specification Generator <devplat-external-sdk-accounts@squareup.com>
2019-01-31 20:58:38 +0000

    Release 2.20181212.0

--- src/test/java/com/squareup/connect/api/CheckoutApiTest.java
+++ src/test/java/com/squareup/connect/api/CheckoutApiTest.java
@@ -136,7 +136,7 @@ public class CheckoutApiTest extends APITest {
                 .firstName("Jane")
                 .lastName("Doe")
             )
-            .redirectUrl("https://merchant.website.com/order-confirm");
+            .redirectUrl("https://docs.connect.squareup.com/order-confirm");
 
         CreateCheckoutResponse response = api.createCheckout(locationId, body);
 
